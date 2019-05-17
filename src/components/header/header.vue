285bd3b4c067fc843e6ffb705a8bcb885225610c
XinweiZhu <995037977@qq.com>
2019-05-17 17:14:37 +0800

    feat   删除多余的console.log

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -83,7 +83,6 @@
             this.classMap = ['decrease','discount','special','invoice','guarantee']
         },
         mounted() {
-            console.log(this.seller.score)
         },
         methods: {
             showDetail(){
